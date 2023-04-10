483c8ef88cad9ecc295b42b0de0904e985117928
Artem <130403999+Art-ad-hub@users.noreply.github.com>
2023-04-11 00:21:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte variableByte = 100;
+            sbyte variableSbyte = - 100;
+            short variableShort = 10000;
+            ushort variableusUshort = 0;
+            int whatYear = 2023;
+            uint age = 30;
+            long maxValue = 9223372036854775807;
+            ulong minMalue = 0;
+            float pi = 3.14f;
+            double variableDouble = 3.14;
+            char tilda = '~';
+            string greeting = "Доброй ночи!";
+            bool likeTraining = true; 
+
+        }
+    }
+}
